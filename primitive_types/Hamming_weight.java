a5720ad26c251e5a912e4179198580914a345c03
MaximilianIsidore <maximilianisidore2003@gmail.com>
2025-04-25 18:35:13 +0530

    add countbits aka hamming weights java

---
+++ primitive_types/Hamming_weight.java
@@ -0,0 +1,14 @@
+
+class Hamming_weight{
+    public static short countBits(int x) {
+        short numBits = 0;
+        while (x != 0 ) {
+            numBits += (x & 1);
+            x >>= 1;
+        }
+        return numBits;
+    }
+    public static void main(String args[]){
+      System.out.println(countBits(15));
+    }
+} 
